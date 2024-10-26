b4f00058c697e5bde65bd6cbccbe8b643b44506e
grafuj <sfrrki@gmail.com>
2024-10-26 13:19:00 -0700

    add resolver functions for 3 entry points

--- graphql-server/src/index.ts
+++ graphql-server/src/index.ts
@@ -1,8 +1,24 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from '@apollo/server/standalone';
 
+import _db from "./_db";
+
 // types
 import { typeDefs } from "./schema";
+
+const resolvers = {
+  Query: {
+    movies() {
+      return _db.movies
+    },
+    reviews() {
+      return _db.reviews
+    },
+    authors() {
+      return _db.authors
+    }
+  }
+}
 
 // server setup
 const port = 4000
@@ -10,7 +26,7 @@ const server = new ApolloServer({
   // typeDefs
   typeDefs,
   // resolvers
-  
+
 
 })
 
